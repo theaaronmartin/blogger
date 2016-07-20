9cb5d54dac1903cbfe250bedb986fc084d8dddaa
Aaron Martin <grav3serker@Aarons-MacBook-Pro.local>
2016-07-20 11:44:47 -0700

    adding CRUD functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,38 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
   def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.new(posts_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
-  def show
+  def update
     @post = Post.find(params[:id])
+
+    if @post.update(posts_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   private
