8e6014ce5e3a142101f096a0df78c14fec4fd1a3
Aaron Martin <grav3serker@Aarons-iMac.local>
2016-07-21 13:53:24 -0700

    adding more to file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,14 @@ class CommentsController < ApplicationController
     redirect_to @post
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to @post
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
